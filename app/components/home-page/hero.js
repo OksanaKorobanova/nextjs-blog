d29b54f607a4b0612d6380704b6b1123ba76290e
Oksana Korobanova <58445023+OksanaKorobanova@users.noreply.github.com>
2023-05-28 17:57:10 +0200

    added main layout, hero, featured posts

---
+++ app/components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import classes from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/logo.webp'
+          alt='My photo'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I&apos;m Oksana</h1>
+      <p>
+        A blog about Front end developments (React, Typescript, Next.js, etc.)
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import FeaturedPosts from './components/home-page/featured-posts';
+import Hero from './components/home-page/hero';
+
+const TEMP_POSTS = [
+  {
+    title: 'Post 1',
+    image: '1.jpg',
+    excerpt: 'Post 1 description',
+    date: '2023-02-10',
+    slug: 'post_1',
+  },
+  {
+    title: 'Post 2',
+    image: '2.jpg',
+    excerpt: 'Post 2 description',
+    date: '2023-03-10',
+    slug: 'post_2',
+  },
+  {
+    title: 'Post 3',
+    image: '3.jpeg',
+    excerpt: 'Post 3 description',
+    date: '2023-04-10',
+    slug: 'post_3',
+  },
+];
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <Hero />
+      <FeaturedPosts posts={TEMP_POSTS} />
+    </>
+  );
 }
